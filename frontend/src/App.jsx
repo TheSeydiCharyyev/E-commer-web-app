cf209c03ea7fc28919686e1fdcd55dd6b59ce8ff
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-12 17:21:05 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px - [9vw]'>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<Home />} />
       </Routes>
     </div>
   )
