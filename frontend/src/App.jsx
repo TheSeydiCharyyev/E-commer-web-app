c75ef96c157c3119a7c7e1187c2f9e16b4445114
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-12 17:27:47 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
+import About from './pages/About'
 
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+
       </Routes>
     </div>
   )
