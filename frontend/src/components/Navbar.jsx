2bac1d2f329414b084cba4c35f0fa070c1708a3d
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-14 08:44:56 +0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { assets } from '../assets/assets'
-
+import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets.logo} className='w-36' alt="" />
+            <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+                <NavLink></NavLink>
+            </ul>
         </div>
     )
 }
