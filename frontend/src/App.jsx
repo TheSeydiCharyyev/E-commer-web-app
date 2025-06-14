116a099f005843f53d9f8a524f01d44d6e0c6679
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-14 08:32:57 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@ import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/Login'
+import Orders from './pages/Orders'
 
 
 const App = () => {
@@ -16,6 +21,8 @@ const App = () => {
         <Route path='/product/:productId' element={<Product />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
       </Routes>
     </div>
   )
