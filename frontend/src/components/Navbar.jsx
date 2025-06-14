2178c3778bb67e1eceeb7f4a4b3f1e3f9dfd9f3b
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-14 08:49:07 +0500

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,9 @@ const Navbar = () => {
         <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets.logo} className='w-36' alt="" />
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
-                <NavLink className={'flex flex-col'}>
-
+                <NavLink className={'flex flex-col items-center gap-1'}>
+                    <p>Home </p>
+                    <hr />
                 </NavLink>
             </ul>
         </div>
