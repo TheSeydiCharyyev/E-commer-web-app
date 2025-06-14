be7ad5bc0fe8bcc1724a07e66e7b6494d793d2ef
Seydi Charyyev <seydi.charyev@gmail.com>
2025-06-14 08:28:10 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,8 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/product/:productId' element={<Product />} />
-
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   )
